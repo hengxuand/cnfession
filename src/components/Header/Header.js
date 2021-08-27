4306df78a5b30f48701652afeeeb4c3629b2505a
hx <hengxuan.dong@gmail.com>
2021-08-27 00:44:54 -0700

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import "./Header.scss";
+
+const Header = () => {
+  return (
+    <div className="header justify-content-between">
+      <div className="title text-uppercase">
+        {/* <i className="bi bi-signpost-split"></i> */}
+        <span className="ps-4">cnfession</span>
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Header;
