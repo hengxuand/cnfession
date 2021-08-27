3d959d2d2c9f5cc268588c8509c0b2ac2b2b1351
hx <hengxuan.dong@gmail.com>
2021-08-27 13:32:05 -0700

    search box change

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,7 @@
 import "./Header.scss";
 
 const Header = () => {
-  return (
-    <div className="header justify-content-between">
-      <div className="title text-uppercase">
-        {/* <i className="bi bi-signpost-split"></i> */}
-        <span className="ps-4">cnfession</span>
-      </div>
-      <div></div>
-    </div>
-  );
+  return <div className="header">cnfession</div>;
 };
 
 export default Header;
--- src/components/SearchBox/SearchBox.js
+++ src/components/SearchBox/SearchBox.js
@@ -2,52 +2,20 @@ import "./SearchBox.scss";
 import { useState } from "react";
 
 const SearchBox = () => {
-  const [showCross, toggleShowCross] = useState(false);
-  const [searchInput, setSearchInput] = useState("");
-
   const handleTyping = (event) => {
-    if (event.target.value.length > 0) {
-      setSearchInput(event.target.value);
-      toggleShowCross(true);
-    } else {
-      setSearchInput("");
-      toggleShowCross(false);
+    if (event.target.value.length > 3) {
     }
 
     console.log(event.target.value);
   };
-  const clearSearch = (event) => {
-    setSearchInput("");
-    toggleShowCross(false);
-  };
 
   return (
-    <div className="row mt-3">
-      <div className="col-md-8 mx-auto">
-        <div className="search_wrapper input-group">
-          <input
-            className="searchBoxFrame form-control"
-            type="text"
-            value={searchInput}
-            placeholder="Find your interest"
-            onInput={handleTyping}
-          ></input>
-          <div className="crossFrame">
-            {showCross && (
-              <button
-                className="btn cross_btn bg-white "
-                onClick={clearSearch}
-                type="button"
-              >
-                <div className="crossing">
-                  <i className="bi bi-x-lg"></i>
-                </div>
-              </button>
-            )}
-          </div>
-        </div>
-      </div>
-    </div>
+    <input
+      className="row col-6 mx-auto searchBox"
+      type="search"
+      placeholder="Find your interest"
+      onInput={handleTyping}
+    ></input>
   );
 };
 
