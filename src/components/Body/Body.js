aa10affc8a87fe65b5750b47f0abc8ba478cbaf4
hx <hengxuan.dong@gmail.com>
2021-08-30 21:33:45 -0700

    fds

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -5,24 +5,30 @@ const Body = () => {
   const data = [
     {
       id: 1,
-      title: "test1 title",
-      content: "test1 content body.",
-      who: "James",
+      title: "malorum corrumpit",
+      content:
+        "Lorem ipsum dolor sit amet, te eum error nullam, et his quem indoctum. Id eum esse omittam periculis, et vis populo efficiantur. Esse quaestio ad quo, ullum omittantur dissentiet an nec. Ea erant dissentiet vim, quo cu forensibus appellantur instructior. Sit et congue adipiscing, at odio inimicus qui, ad ius malorum corrumpit. No per semper reprimique.",
+      who: "James Canvas",
       date: "2021-08-27",
+      tags: ["interview", "job", "career"],
     },
     {
       id: 2,
-      title: "test2 title",
-      content: "test2 content body.",
-      who: "Mike",
+      title: "forensibus appellantur",
+      content:
+        "Ea intellegat constituto dissentiet quo, ea possim hendrerit vim. Et minim everti reprehendunt has. Verear facilisi ex mei, ius eu graece corpora adversarium. Et graeci propriae antiopam ius. Cetero aperiam intellegebat sea in, eos cu tollit scriptorem. Fuisset detraxit comprehensam pri ei.",
+      who: "Mike Smith",
       date: "2021-08-21",
+      tags: ["tag4", "tag5", "tag6"],
     },
     {
       id: 3,
-      title: "test3 title",
-      content: "test3 content body.",
-      who: "Colin",
+      title: "oportere incorrupte",
+      content:
+        "Ut amet reformidans concludaturque per, sit ex oportere incorrupte. Sea ea dico accusam expetendis, elit denique detraxit usu et, et has partem blandit gubergren. Partem dictas regione pri cu, qui dico veri recteque no, no nec mundi vocibus commune. Mediocrem partiendo id nec, vel at soluta voluptatibus.",
+      who: "Colin Forum",
       date: "2021-02-07",
+      tags: ["tag7", "tag8", "tag9"],
     },
   ];
   const dataList = data.map((itemData, index) => (
@@ -33,6 +39,7 @@ const Body = () => {
       content={itemData.content}
       who={itemData.who}
       date={itemData.date}
+      tags={itemData.tags}
     ></Item>
   ));
   return (
--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,18 +1,48 @@
+import { useState } from "react";
 import "./Item.scss";
 
 const Item = (props) => {
+  const [showContent, changeContent] = useState(false);
+
+  const handleClick = (event) => {
+    if (showContent) {
+      changeContent(false);
+    } else {
+      changeContent(true);
+    }
+  };
+
   return (
-    <table className="row col-6 mx-auto item">
-      <tr>
-        <td>{props.id}</td>
-        <td>{props.title}</td>
-        <td>{props.content}</td>
-        <table>
-          <td>{props.who}</td>
-          <td>{props.date}</td>
-        </table>
-      </tr>
-    </table>
+    <div className="row col-6 d-flex mx-auto item" role="presentation">
+      <div className="col-1 d-flex">
+        <div className="text-muted">#</div>
+        {props.id}
+      </div>
+      <div className="col-11 align-items-start d-flex flex-column">
+        <div className="">{props.who}</div>
+        <div className="story_date text-muted">{props.date}</div>
+        <button className="story_title" onClick={handleClick}>
+          {props.title}
+        </button>
+
+        <div className="story_content">{showContent ? props.content : ""}</div>
+
+        <div className="story_tags">
+          <div className="d-inline-flex ">
+            <div className="text-muted">#</div>
+            {props.tags[0]}&nbsp;&nbsp;
+          </div>
+          <div className="d-inline-flex">
+            <div className="text-muted">#</div>
+            {props.tags[1]}&nbsp;&nbsp;
+          </div>
+          <div className="d-inline-flex">
+            <div className="text-muted">#</div>
+            {props.tags[2]}
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
